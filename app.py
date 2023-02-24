38e1bf4d3ee360c96fb1d6df81ba3ee96b6a2c97
Eran Sidi <eran_cd@outlook.com>
2023-02-24 21:58:49 +0000

    remove wrong file

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-import docker
+from docker_connect import docker_connect
+
+
+
+docker = docker_connect()
+
+print(docker.get_all_containers_names())
+docker.remove_container('unruffled_almeida')
+print(docker.get_all_containers_names())
